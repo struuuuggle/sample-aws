cefaff802bbe94cf3a666390ae9667e8d58142f4
struuuuggle <mikiya.abe@mixi.co.jp>
2024-08-29 14:39:59 +0900

    chore: update tags#Name

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ resource "aws_instance" "app_server" {
   instance_type = "t2.micro"
 
   tags = {
-    Name = "Handson_MikiyaAbe"
+    Name = "Handson_MikiyaAbe-2"
     TED = "20240829_Handson"
   }
 }
